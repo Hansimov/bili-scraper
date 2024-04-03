fde4317b2c0f562978aa87c90ec4f82c5a94eb6f
Hansimov <591172499@qq.com>
2024-04-04 03:54:15 +0800

    :gem: [Feature] New ProxyBenchmarker

--- networks/proxy_pool.py
+++ networks/proxy_pool.py
@@ -1,7 +1,8 @@
 import ast
+import concurrent.futures
 import requests
 
-from tclogger import logger, OSEnver
+from tclogger import logger, OSEnver, Runtimer
 from pathlib import Path
 
 from .constants import REQUESTS_HEADERS
@@ -11,9 +12,6 @@ class ProxyPool:
     def __init__(self):
         enver = OSEnver(Path(__file__).parents[1] / "configs" / "secrets.json")
         self.proxy_endpoint = enver["proxy_endpoint"]
-        self.test_url = (
-            "http://api.bilibili.com/x/web-interface/newlist?rid=95&pn=1&ps=1"
-        )
 
     def get_proxies_list(self):
         logger.note(f"> Fetching proxies list from {self.proxy_endpoint}", end=" ")
@@ -28,27 +26,72 @@ class ProxyPool:
             data = []
         return data
 
-    def bench(self, proxies: list[str] = []):
-        for proxy in proxies:
-            logger.note(f"> Benchmarking proxy: http://{proxy}", end=" ")
-            requests_proxies = {
-                "http": f"http://{proxy}",
-                "https": f"http://{proxy}",
-            }
-            response = requests.get(
+
+class ProxyBenchmarker:
+    def __init__(self):
+        self.test_url = (
+            "http://api.bilibili.com/x/web-interface/newlist?rid=95&pn=1&ps=1"
+        )
+
+        self.tested_count = 0
+        self.success_count = 0
+        self.success_proxies = []
+        self.timer = Runtimer(is_log=False)
+
+    def test_proxy(self, proxy=None):
+        self.timer.start_time()
+        self.tested_count += 1
+        count_str = f"[{self.tested_count}/{self.total_count}]"
+        try:
+            res = requests.get(
                 self.test_url,
                 headers=REQUESTS_HEADERS,
-                timeout=5,
-                proxies=requests_proxies,
+                proxies={
+                    "http": f"http://{proxy}",
+                    "https": f"http://{proxy}",
+                },
+                timeout=1,
             )
-            if response.status_code == 200:
-                logger.success(f"[{response.status_code}]")
-            else:
-                logger.warn(f"[{response.status_code}]")
+        except:
+            logger.err(f"{count_str} × Not Connected: {proxy}")
+            return False
+
+        self.timer.end_time()
+        elapsed_time = self.timer.elapsed_time()
+        elapsed_time_str = self.timer.time2str(elapsed_time, unit_sep="")
+
+        try:
+            data = res.json()
+            logger.success(f"{count_str} √ OK: [{elapsed_time_str}] {proxy}")
+            self.success_count += 1
+            self.success_proxies.append(proxy)
+        except:
+            logger.err(f"{count_str} × [{res.status_code}] {proxy}")
+            return False
+
+        return True
+
+    def batch_test_proxy(self, proxies):
+        logger.note(f"> Benchmarking {len(proxies)} proxies")
+        self.total_count = len(proxies)
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [executor.submit(self.test_proxy, proxy) for proxy in proxies]
+
+        for idx, future in enumerate(concurrent.futures.as_completed(futures)):
+            result = future.result()
+
+        logger.success(self.success_count, end="")
+        logger.note(f"/{self.total_count}")
+
+        if self.success_proxies:
+            logger.success(self.success_proxies)
 
 
 if __name__ == "__main__":
     proxy_pool = ProxyPool()
     proxies = proxy_pool.get_proxies_list()
+
+    benchmarker = ProxyBenchmarker()
+    benchmarker.batch_test_proxy(proxies[:])
 
     # python -m networks.proxy_pool
