dba5ba6da73b49a81f90e7b1342f07e440344c20
Hansimov <591172499@qq.com>
2024-05-09 01:17:08 +0800

    :gem: [Feature] VideoInfoConverter: flatten, rename and insert time for video info

--- transforms/video_row.py
+++ transforms/video_row.py
@@ -1,4 +1,12 @@
-class VideoInfoItem:
+import requests
+
+from datetime import datetime
+from tclogger import logger
+
+from networks.constants import REQUESTS_HEADERS, GET_VIDEO_PAGE_API
+
+
+class VideoInfoConverter:
     """
     - 视频 API 信息
       - https://socialsisteryi.github.io/bilibili-API-collect/docs/video/info.html#%E8%8E%B7%E5%8F%96%E8%A7%86%E9%A2%91%E8%AF%A6%E7%BB%86%E4%BF%A1%E6%81%AF-web%E7%AB%AF
@@ -92,4 +100,56 @@ class VideoInfoItem:
     }
 
     def __init__(self):
-        pass
+        self.rename_columns()
+
+    def rename_columns(self):
+        self.COLUMNS = {
+            self.COLUMNS_RENAME_MAP.get(k, k): v for k, v in self.COLUMNS.items()
+        }
+
+    def flatten(self, video_info: dict):
+        new_video_info = {}
+        for k, v in video_info.items():
+            if k in ["owner", "stat"]:
+                new_video_info.update(v)
+            else:
+                new_video_info[k] = v
+        return new_video_info
+
+    def rename(self, video_info: dict):
+        new_video_info = {}
+        for k, v in video_info.items():
+            if k in self.COLUMNS_RENAME_MAP:
+                new_k = self.COLUMNS_RENAME_MAP[k]
+                new_video_info[new_k] = v
+            else:
+                new_video_info[k] = v
+        return new_video_info
+
+    def get_now_timestamp(self):
+        return int(datetime.now().timestamp())
+
+    def to_sql_row(self, video_info: dict):
+        new_video_info = video_info
+        new_video_info = self.flatten(new_video_info)
+        new_video_info = self.rename(new_video_info)
+        new_video_info["inserted_at"] = self.get_now_timestamp()
+        return new_video_info
+
+
+if __name__ == "__main__":
+    res = requests.get(
+        GET_VIDEO_PAGE_API,
+        headers=REQUESTS_HEADERS,
+        params={"rid": 95, "pn": 1000, "ps": 50},
+    )
+    if res.status_code == 200:
+        res_json = res.json()
+
+    video_info_dict = res_json["data"]["archives"][0]
+
+    converter = VideoInfoConverter()
+    new_video_info = converter.to_sql_row(video_info_dict)
+    logger.mesg(new_video_info)
+
+    # python -m transforms.video_row
