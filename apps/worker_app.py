2777ac9649b82594cae50391933f091600d68822
Hansimov <591172499@qq.com>
2024-05-18 10:14:42 +0800

    :recycle: [Refactor] Separate WorkerParamsGenerator and Worker from WorkerApp

--- apps/worker_app.py
+++ apps/worker_app.py
@@ -1,257 +1,19 @@
-import asyncio
 import concurrent.futures
 import requests
 import threading
-import time
 import uvicorn
 
 from fastapi import FastAPI, Body
-from random import randint
-from tclogger import logger, Runtimer
-from typing import Optional, Literal
+from tclogger import logger
+from typing import Optional
 
 
 from apps.arg_parser import ArgParser
-from configs.envs import WORKER_APP_ENVS, VIDEO_PAGE_API_MOCKER_ENVS, PROXY_APP_ENVS
-from networks.constants import REQUESTS_HEADERS, GET_VIDEO_PAGE_API, REGION_CODES
+from configs.envs import WORKER_APP_ENVS, PROXY_APP_ENVS
+from workers.worker import WorkerParamsGenerator, Worker
 
 
-class WorkerParamsGenerator:
-    def __init__(self, region_codes: list[str] = [], mock: bool = False):
-        self.mock = mock
-        self.region_codes = region_codes
-
-        self.init_tids()
-
-    def init_tids(self):
-        self.tid_idx = 0
-        main_regions = [REGION_CODES[region_code] for region_code in self.region_codes]
-        self.regions = [
-            region
-            for main_region in main_regions
-            for region in main_region["children"].values()
-        ]
-        self.tids = [region["tid"] for region in self.regions]
-        self.is_current_region_exhausted = False
-        self.pn = 0
-        self.tid = self.get_tid()
-        logger.note(f"> Regions: {self.region_codes} => {len(self.tids)} sub-regions")
-
-    def get_region(self):
-        if self.tid_idx < len(self.regions):
-            return self.regions[self.tid_idx]
-        else:
-            return {}
-
-    def get_tid(self):
-        if self.tid_idx < len(self.tids):
-            return self.tids[self.tid_idx]
-        else:
-            return -1
-
-    def flag_current_region_exhausted(self):
-        self.is_current_region_exhausted = True
-
-    def is_terminated(self):
-        if self.tid == -1 and self.pn == -1:
-            return True
-        return False
-
-    def next(self):
-        if not self.is_current_region_exhausted:
-            self.pn += 1
-        else:
-            self.tid_idx += 1
-            if self.tid_idx < len(self.regions):
-                self.tid = self.get_tid()
-                self.pn = 1
-            else:
-                self.tid = -1
-                self.pn = -1
-            self.is_current_region_exhausted = False
-        return self.tid, self.pn
-
-
-class ResponseCategorizer:
-    def categorize(
-        self, res_dict: dict
-    ) -> Literal["network_error", "end_of_region", "normal"]:
-        code = res_dict.get("code")
-        if code != 0:
-            return "network_error"
-        archives = res_dict.get("data", {}).get("archives", [])
-        if len(archives) == 0:
-            return "end_of_region"
-        else:
-            return "normal"
-
-
-class Worker:
-    def __init__(
-        self,
-        generator: WorkerParamsGenerator,
-        lock,
-        wid: int = -1,
-        proxy: str = None,
-        mock: bool = False,
-        interval: float = 2.5,
-        retry_count: int = 3,
-        time_out: float = 2.0,
-    ):
-        self.wid = wid
-        self.generator = generator
-        self.lock = lock
-        self.proxy = proxy
-        self.mock = mock
-        self.active = False
-        self.interval = interval
-        self.retry_count = retry_count
-        self.time_out = time_out
-        self.response_categorizer = ResponseCategorizer()
-        self.proxy_endpoint = f"http://127.0.0.1:{PROXY_APP_ENVS['port']}"
-        self.get_proxy_api = f"{self.proxy_endpoint}/get_proxy"
-        self.drop_proxy_api = f"{self.proxy_endpoint}/drop_proxy"
-
-    def get_proxy(self):
-        # LINK apps/proxy_app.py#get_proxy
-        params = {"mock": self.mock}
-        try:
-            res = requests.get(self.get_proxy_api, params=params)
-            if res.status_code == 200:
-                proxy = res.json().get("server")
-            else:
-                proxy = None
-        except Exception as e:
-            proxy = None
-
-        if proxy:
-            if not self.proxy:
-                logger.file(f"> New worker {self.wid} with proxy: [{proxy}]")
-            else:
-                logger.file(f"> Worker {self.wid} with new proxy: [{proxy}]")
-            self.active = True
-        else:
-            logger.warn(f"× Failed to create new worker: No usable proxy")
-            self.active = False
-
-        self.proxy = proxy
-
-    def drop_proxy(self):
-        # LINK apps/proxy_app.py#drop_proxy
-        try:
-            requests.post(self.drop_proxy_api, json={"server": self.proxy})
-        except Exception as e:
-            pass
-
-    def get_page(self, tid: int, pn: int, ps: int = 50):
-        proxy = self.proxy
-        mock = self.mock
-
-        if proxy:
-            proxies = {"http": f"http://{proxy}", "https": f"http://{proxy}"}
-        else:
-            proxies = None
-
-        if mock:
-            url = f"http://127.0.0.1:{VIDEO_PAGE_API_MOCKER_ENVS['port']}/page_info"
-            proxies = None
-        else:
-            url = GET_VIDEO_PAGE_API
-
-        params = {"rid": tid, "pn": pn, "ps": ps}
-
-        res_json = None
-        retry_count = 0
-        while retry_count < self.retry_count:
-            try:
-                res = requests.get(
-                    url,
-                    headers=REQUESTS_HEADERS,
-                    params=params,
-                    proxies=proxies,
-                    timeout=self.time_out,
-                )
-                if res.status_code == 200:
-                    res_json = res.json()
-                    break
-                else:
-                    retry_count += 1
-            except Exception as e:
-                retry_count += 1
-
-        if not res_json:
-            res_json = {
-                "code": -1,
-                "message": f"Failed to get page after {self.retry_count} retries: tid={tid}, pn={pn}",
-                "data": {
-                    "archives": [],
-                    "page": {"count": -1, "num": pn, "size": ps},
-                },
-            }
-
-        return res_json
-
-    def run(self):
-        if not self.proxy:
-            self.get_proxy()
-
-        retry_last_params = False
-        self.timer = Runtimer(verbose=False)
-
-        while True:
-            self.timer.start_time()
-            if not self.active:
-                break
-
-            with self.lock:
-                if self.generator.is_terminated():
-                    self.active = False
-                    break
-
-            with self.lock:
-                if not retry_last_params:
-                    tid, pn = self.generator.next()
-                else:
-                    tid, pn = tid, pn
-                    retry_last_params = False
-
-            region_name = self.generator.get_region().get("name", "Unknown")
-            task_str = f"wid={self.wid}, tid={tid}, pn={pn}, region={region_name}"
-            logger.note(f"> GET: {task_str}")
-
-            if tid == -1 and pn == -1:
-                logger.success(f"[Finished]")
-                break
-
-            res_json = self.get_page(tid=tid, pn=pn)
-            archives = res_json.get("data", {}).get("archives", [])
-            res_code = res_json.get("code", -1)
-            res_condition = self.response_categorizer.categorize(res_json)
-
-            if res_condition == "end_of_region":
-                logger.success(f"  + End of region: {region_name}")
-                with self.lock:
-                    self.generator.flag_current_region_exhausted()
-            elif res_condition == "normal":
-                logger.success(f"  + GOOD: {task_str}", end=" ")
-                logger.mesg(f"<{len(archives)} videos>")
-            elif res_condition == "network_error":
-                logger.warn(f"  × BAD: {task_str} [code={res_code}]")
-                logger.warn(f"    {res_json.get('message', '')}")
-                self.drop_proxy()
-                self.get_proxy()
-                retry_last_params = True
-            else:
-                logger.warn(f"  - Unknown condition: {task_str} [code={res_code}]")
-
-            self.timer.end_time()
-            dt = self.timer.elapsed_time()
-
-            sleep_time = max(self.interval - dt.total_seconds(), 0)
-            time.sleep(sleep_time)
-
-
-class WorkersApp:
+class WorkerApp:
     def __init__(self):
         self.app = FastAPI(
             docs_url="/",
@@ -323,7 +85,7 @@ class WorkersApp:
         )(self.stop)
 
 
-app = WorkersApp().app
+app = WorkerApp().app
 
 if __name__ == "__main__":
     args = ArgParser(app_envs=WORKER_APP_ENVS).args
