753f13f2d0ee816d72ee76b0800a352e9b265ee6
Hansimov <591172499@qq.com>
2024-06-04 02:42:43 +0800

    :recycle: [Refactor] ParamsWBISigner: prettify codes, args and logs

--- networks/wbi.py
+++ networks/wbi.py
@@ -12,6 +12,7 @@ import requests
 from functools import reduce
 from hashlib import md5
 from pprint import pformat
+from tclogger import logger
 
 from configs.envs import COOKIES
 from networks.constants import REQUESTS_HEADERS
@@ -38,26 +39,28 @@ class ParamsWBISigner:
         }
         self.headers.update(new_headers)
 
+        self.img_key = None
+        self.sub_key = None
+
     def get_magic_key(self, text: str):
         """shuffle img_key and sub_key"""
         return reduce(lambda s, i: s + text[i], self.MAGIC_KEYS, "")[:32]
 
     def get_wbi_keys(self) -> tuple[str, str]:
         """get latest img_key and sub_key"""
-        resp = requests.get(
-            "https://api.bilibili.com/x/web-interface/nav", headers=self.headers
-        )
+        nav_url = "https://api.bilibili.com/x/web-interface/nav"
+        resp = requests.get(nav_url, headers=self.headers)
         resp.raise_for_status()
         json_content = resp.json()
-        img_url: str = json_content["data"]["wbi_img"]["img_url"]
-        sub_url: str = json_content["data"]["wbi_img"]["sub_url"]
-        img_key = img_url.rsplit("/", 1)[1].split(".")[0]
-        sub_key = sub_url.rsplit("/", 1)[1].split(".")[0]
-        return img_key, sub_key
+        wbi_img = json_content["data"]["wbi_img"]
+        img_url = wbi_img["img_url"]
+        sub_url = wbi_img["sub_url"]
+        self.img_key = img_url.rsplit("/", 1)[1].split(".")[0]
+        self.sub_key = sub_url.rsplit("/", 1)[1].split(".")[0]
 
     def sign_params_with_wbi(self, params: dict, img_key: str, sub_key: str):
         """sign params with wbi"""
-        mixin_key = self.get_magic_key(img_key + sub_key)
+        magic_key = self.get_magic_key(img_key + sub_key)
         curr_time = round(time.time())
         params["wts"] = curr_time
         params = dict(sorted(params.items()))
@@ -66,25 +69,31 @@ class ParamsWBISigner:
             for k, v in params.items()
         }
         query = urllib.parse.urlencode(params)
-        wbi_sign = md5((query + mixin_key).encode()).hexdigest()
+        wbi_sign = md5((query + magic_key).encode()).hexdigest()
         params["w_rid"] = wbi_sign
         return params
 
     def run(self, params: dict):
-        img_key, sub_key = self.get_wbi_keys()
-        signed_params = self.sign_params_with_wbi(params, img_key, sub_key)
+        if self.img_key is None or self.sub_key is None:
+            self.get_wbi_keys()
+        signed_params = self.sign_params_with_wbi(params, self.img_key, self.sub_key)
         return signed_params
 
 
 if __name__ == "__main__":
     url = "https://api.bilibili.com/x/space/wbi/arc/search"
-    params = {"mid": 946974, "pn": 2, "ps": 5}
+    mid = 946974
+    pn, ps = 1, 5
+    params = {"mid": mid, "pn": pn, "ps": ps}
     signer = ParamsWBISigner()
     signed_params = signer.run(params)
+    logger.note("> Get signed params:")
+    print(pformat(signed_params, sort_dicts=False))
     headers = signer.headers
     headers["Cookie"] = COOKIES
     res = requests.get(url, headers=headers, params=signed_params)
     res.raise_for_status()
+    logger.note(f"> Get videos from mid {mid}:")
     print(pformat(res.json(), sort_dicts=False))
 
     # python -m networks.wbi
