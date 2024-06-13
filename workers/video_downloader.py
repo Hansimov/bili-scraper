005f649a80d2336e893e79fa1e7894914b7253c1
Hansimov <591172499@qq.com>
2024-06-14 00:58:11 +0800

    :zap: [Enhance] VideoDownloader: check_cache and save_meta_to_json

--- workers/video_downloader.py
+++ workers/video_downloader.py
@@ -1,3 +1,5 @@
+import json
+
 from pathlib import Path
 
 from tclogger import logger, shell_cmd
@@ -6,12 +8,12 @@ from configs.envs import VIDEOS_ENVS, COOKIES_DICT
 
 
 class VideoDownloader:
-    def __init__(self):
-        pass
-
     def get_cmd_args(self, bvid: str, mid: int = 0):
         self.bbdown = "BBDown"
+        self.bvid = bvid
+        self.mid = mid
         self.user_videos_dir = Path(VIDEOS_ENVS["root"]) / f"{mid}"
+        self.user_videos_meta_json = Path(VIDEOS_ENVS["root"]) / f"{mid}" / "meta.json"
 
         if not self.user_videos_dir.exists():
             self.user_videos_dir.mkdir(parents=True, exist_ok=True)
@@ -30,12 +32,46 @@ class VideoDownloader:
         self.cmd_str = f'{self.bbdown} "{bvid}" {self.cmd_args_str}'
         return self.cmd_str
 
+    def check_cache(self):
+        if not self.user_videos_meta_json.exists():
+            return False
+        else:
+            with open(self.user_videos_meta_json, "r") as rf:
+                meta_dict = json.load(rf)
+            if meta_dict.get(self.bvid, {}).get("status", "") == "ok":
+                cached_files = meta_dict.get(self.bvid, {}).get("files", [])
+                logger.mesg(
+                    f"  * {len(cached_files)} files cached for bvid: [{self.bvid}]"
+                )
+                logger.file(f"  * {cached_files}")
+                return True
+        return False
+
+    def save_meta_to_json(self):
+        if not self.user_videos_meta_json.exists():
+            meta_dict = {}
+        else:
+            with open(self.user_videos_meta_json, "r") as rf:
+                meta_dict = json.load(rf)
+
+        files_start_with_bvid = sorted(
+            [f.name for f in self.user_videos_dir.glob(f"{self.bvid}*")]
+        )
+        meta_dict[self.bvid] = {"status": "ok", "files": files_start_with_bvid}
+
+        with open(self.user_videos_meta_json, "w", encoding="utf-8") as wf:
+            json.dump(meta_dict, wf, ensure_ascii=False, indent=4)
+
+        logger.success(f"+ meta json saved for bvid: [{self.bvid}]")
+
     def download(self, bvid: str, mid: int = 0):
         self.get_cmd_args(bvid, mid)
         logger.note(f"> Download video: [{bvid}]")
-        logger.file(f"  - {self.user_videos_dir}")
-        # logger.mesg(f"  * {self.cmd_str}")
-        shell_cmd(self.cmd_str)
+        logger.file(f"  - {self.user_videos_dir}/{bvid}*")
+        is_cached = self.check_cache()
+        if not is_cached:
+            shell_cmd(self.cmd_str)
+            self.save_meta_to_json()
 
 
 if __name__ == "__main__":
