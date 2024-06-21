7209e3793e9c02189289863f688465fbcd72c2e1
Hansimov <591172499@qq.com>
2024-06-22 04:47:15 +0800

    :zap: [Enhance] ProxyBenchmarker: determine test_url and success condition by test_type

--- networks/proxy_pool.py
+++ networks/proxy_pool.py
@@ -5,7 +5,7 @@ import requests
 import time
 
 from tclogger import logger, int_bits, OSEnver, Runtimer
-from pathlib import Path
+from typing import Literal
 
 from networks.constants import REQUESTS_HEADERS
 from configs.envs import SECRETS
@@ -31,10 +31,16 @@ class ProxyPool:
 
 
 class ProxyBenchmarker:
-    def __init__(self):
-        self.test_url = (
-            "http://api.bilibili.com/x/web-interface/newlist?rid=95&pn=1&ps=1"
-        )
+    def __init__(self, test_type: Literal["newlist", "view"] = "newlist"):
+        self.test_type = test_type
+        if self.test_type == "view":
+            self.test_url = (
+                "http://api.bilibili.com/x/web-interface/view?bvid=BV1Qz421B7W9"
+            )
+        else:
+            self.test_url = (
+                "http://api.bilibili.com/x/web-interface/newlist?rid=95&pn=1&ps=1"
+            )
 
         self.tested_count = 0
         self.success_count = 0
@@ -44,6 +50,7 @@ class ProxyBenchmarker:
         self.retry_count = 5
         self.retry_interval = 0.1
         self.accept_success_rate = 0.2
+        self.test_timeout = 1
 
     def test_proxy(self, proxy=None, good_callback=None, bad_callback=None):
         self.tested_count += 1
@@ -61,14 +68,24 @@ class ProxyBenchmarker:
                     self.test_url,
                     headers=REQUESTS_HEADERS,
                     proxies={"http": f"http://{proxy}", "https": f"http://{proxy}"},
-                    timeout=1,
+                    timeout=self.test_timeout,
                 )
                 self.timer.end_time()
                 elapsed_time = self.timer.elapsed_time()
                 elapsed_seconds = round(elapsed_time.microseconds * 1e-6, 4)
                 res_json = res.json()
-                archives = res_json.get("data", {}).get("archives", [])
-                if len(archives) > 0:
+
+                is_success = False
+                if self.test_type == "view":
+                    data = res_json.get("data", None)
+                    if data:
+                        is_success = True
+                else:
+                    archives = res_json.get("data", {}).get("archives", [])
+                    if len(archives) > 0:
+                        is_success = True
+
+                if is_success:
                     current_proxy_success_count += 1
                     current_proxy_accumulated_time += elapsed_seconds
             except:
@@ -107,6 +124,7 @@ class ProxyBenchmarker:
 
     def batch_test_proxy(self, proxies, good_callback=None, bad_callback=None):
         logger.note(f"> Benchmarking {len(proxies)} proxies")
+        logger.file(f"  * test_url: {self.test_url}")
         self.total_count = len(proxies)
         with concurrent.futures.ThreadPoolExecutor(
             max_workers=self.max_workers
